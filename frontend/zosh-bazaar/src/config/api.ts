004360a260953b4b1c2ecf36b49745fc2686a7f3
Mohd Aaftab <aaftabansari034@gmail.com>
2026-05-12 22:54:44 +0530

    changes

--- frontend/zosh-bazaar/src/config/api.ts
+++ frontend/zosh-bazaar/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BACKEND_URL;
+const baseURL = import.meta.env.VITE_BACKEND_URL || "/api";
 
 export const api = axios.create({
   baseURL,
